e2324498bc1e70eacf397323d13182b0d0bc3f28
Ayan Das <ayan@home-mbp.lan>
2021-01-03 23:20:56 -0500

    Added a Python file for getting search engine data

---
+++ data_creation.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import os
+
+if not os.path.exists("./data"):
+    os.makedirs("./data")
+
+# Get a list of Pokemon
+pokemon_total_count = requests.get("https://pokeapi.co/api/v2/pokemon").json()['count']
+
+pokemons = []
+offset=0
+while len(pokemons) != pokemon_total_count:
+    pokemonResults = requests.get("https://pokeapi.co/api/v2/pokemon?limit=100&offset={}".format(offset)).json()
+    for result in pokemonResults["results"]:
+        url = result["url"]
+        i = url.split("/")[-2]
+        pokemon = requests.get(url).json()
+        datum = {}
+        datum["id"] = pokemon["id"]
+        datum["name"] = pokemon["name"].upper().replace("-", " ")
+        for stat in pokemon["stats"]:
+            stat_name = stat["stat"]["name"].replace("-", "_")
+            stat_val = stat["base_stat"]
+            datum[stat_name] = stat_val
+        datum["species"] = pokemon['species']['url'].split("/")[-2]
+        pokemons.append(datum)
+        print("Done with Pokemon # {}".format(i))
+    offset += 100
+
+
+with open("./data/pokemon.json", "w") as outFile:
+    json.dump(pokemons, outFile, indent=2)
+
+print("Done with Pokemon")
